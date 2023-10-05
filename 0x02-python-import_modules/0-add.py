4114854de7d7c1c0e9f82d6feff525a88bf2edd6
filili75 <elkaroini.yassine@gmail.com>
2023-10-05 11:49:01 +0100

    python project

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print a sum of 1 and 2."""
+    from add_0 import add
+
+    y = 1
+    c = 2
+    print("{} + {} = {}".format(y, c, add(y, c)))
---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    cont = len(sys.argv) - 1
+    if cont == 0:
+        print("0 arguments.")
+    elif cont == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(cont))
+    for i in range(cont):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
