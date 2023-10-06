f8daa3e440206b5100f66494271a822e494e2df1
filili75 <elkaroini.yassine@gmail.com>
2023-10-06 13:44:28 +0100

    python project

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,12 +1,1 @@
-#!/usr/bin/python3
-def add(a, b):
-    """My addition functions
-
-    Args:
-        a: first integers
-        b: second integers
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+__import__("0-add")
